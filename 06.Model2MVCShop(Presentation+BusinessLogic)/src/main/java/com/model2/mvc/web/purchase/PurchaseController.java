fa020af9d0cea280cf8ada93d354e36d4a3f9b56
hy3in <hy3in01@gmail.com>
2020-11-21 17:33:59 +0900

    ..

--- 06.Model2MVCShop(Presentation+BusinessLogic)/src/main/java/com/model2/mvc/web/purchase/PurchaseController.java
+++ 06.Model2MVCShop(Presentation+BusinessLogic)/src/main/java/com/model2/mvc/web/purchase/PurchaseController.java
@@ -2,9 +2,12 @@ package com.model2.mvc.web.purchase;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.model2.mvc.service.purchase.PurchaseService;
 
+@Controller
 public class PurchaseController {
 	
 	///Field
@@ -12,6 +15,11 @@ public class PurchaseController {
 	@Qualifier("purchaseServiceImpl")
 	private PurchaseService purchaseService;
 	
+	///Constructor
+	public PurchaseController() {
+		System.out.println("==>PurchaseController default Constructor call..");
+	}
 	
+	//@RequestMapping("")
 
 }
